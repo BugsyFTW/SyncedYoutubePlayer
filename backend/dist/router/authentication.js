dbbcaf4cd1d86cfb457947acfe344f32b19caf86
BugsyFTW <bugsyftw@gmail.com>
2023-12-15 19:35:56 +0000

    Deleted dist folder

--- backend/dist/router/authentication.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const constants_1 = require("../config/constants");
-const authentication_1 = require("../controllers/authentication");
-exports.default = (router) => {
-    router.post(`${constants_1.AUTH_PATH}/register`, authentication_1.register);
-    router.post(`${constants_1.AUTH_PATH}/login`, authentication_1.login);
-};
-//# sourceMappingURL=authentication.js.map
