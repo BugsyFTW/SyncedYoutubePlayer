00fabb0159ec3d38277177b304798efda43b2902
BugsyFTW <bugsyftw@gmail.com>
2024-01-19 16:56:33 +0000

    Changes

--- backend/src/config/variables.ts
+++ backend/src/config/variables.ts
@@ -2,7 +2,7 @@ const envVariables = process.env;
 
 const variables = {
     getPort: () => envVariables.PORT,
-    getMongoURL: () => `mongodb+srv://${envVariables.DB_USER}:${envVariables.DB_PASSWORD}@cluster0.7ir2afg.mongodb.net/?retryWrites=true&w=majority`,
+    getMongoURL: () => `mongodb+srv://${envVariables.DB_USER}:${envVariables.DB_PASSWORD}@syncedytplayer.ikz9hco.mongodb.net/?retryWrites=true&w=majority`,
     getAppSecret: () => envVariables.APP_SECRET || '0x0',
     getCookieName: () => envVariables.COOKIE_NAME || "_session",
     getOrigin: () => "http://localhost:8080",
