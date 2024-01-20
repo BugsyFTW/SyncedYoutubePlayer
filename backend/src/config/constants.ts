2e9eccafefbe878b162f7a44695813c1c9e8a777
BugsyFTW <bugsyftw@gmail.com>
2024-01-20 00:58:45 +0000

    Added socket events

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -7,6 +7,15 @@ export const AUTH_PATH = '/auth';
 export const USER_PATH = '/user';
 export const ROOM_PATH = '/room';
 
+export enum SocketEvent {
+  CONNECTION = 'connection',
+  DISCONNECT = 'disconnect',
+  USER_CONNECTION = 'user_connection',
+  LOADING_VIDEO = 'loading_video',
+  LOAD_VIDEO = 'load_video',
+  STATE_CHANGE = 'state_change',
+}
+
 // Event names
 export const ON_USER_CONNECTION = 'user_connection';
 export const ON_OWNER_VIDEO_CHANGED = 'owner_video_changed';
--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -2,6 +2,19 @@ export const PLAYER_ID = 'Synced-YouTube-Player';
 export const ID_REGEX = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
 
 // Event names
-export const ON_USER_CONNECTION = 'user_connection';
+
+export enum SocketEvent {
+  // Connection
+  CONNECTION = 'connect',
+  USER_CONNECTED = 'user_connection',
+  CONNECTION_ERROR = 'connect_error',
+  // Video loading
+  LOADING_VIDEO = 'loading_video',
+  LOAD_VIDEO = 'load_video',
+  // Video state change
+  STATE_CHANGE = 'state_change',
+}
+
+//export const ON_USER_CONNECTION = 'user_connection';
 export const ON_OWNER_VIDEO_CHANGED = 'owner_video_changed';
 export const ON_VIDEO_CHANGED = 'video_changed';
